bdf6a77e1ffdbca6a3d5c7bcc016868fe825e0de
Dániel Stein <daniel@stein.hu>
2016-04-30 18:48:34 +0200

    SVG, PNG export

--- src/main/java/hu/bme/mit/codemodel/rifle/resources/ExportGraph.java
+++ src/main/java/hu/bme/mit/codemodel/rifle/resources/ExportGraph.java
@@ -12,6 +12,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.StreamingOutput;
+import java.io.*;
 
 /**
  * Created by steindani on 3/2/16.
@@ -57,36 +58,7 @@ public class ExportGraph {
             Transaction transaction = dbServices.beginTx();
 
             StreamingOutput stream = output -> {
-
-                GraphvizWriter writer = new GraphvizWriter();
-                writer.emit(output,
-                        new Walker() {
-                            @Override
-                            public <R, E extends Throwable> R accept(Visitor<R, E> visitor) throws E {
-                                for (Node node : dbServices.graphDb.getAllNodes()) {
-                                    if (node.hasLabel(Label.label("CompilationUnit"))) {
-                                        continue;
-                                    }
-                                    if (node.hasLabel(Label.label("SourceSpan"))) {
-                                        continue;
-                                    }
-                                    if (node.hasLabel(Label.label("SourceLocation"))) {
-                                        continue;
-                                    }
-
-                                    visitor.visitNode(node);
-                                    for (Relationship edge : node.getRelationships(Direction.OUTGOING)) {
-                                        if (edge.isType(RelationshipType.withName("location"))) {
-                                            continue;
-                                        }
-                                        visitor.visitRelationship(edge);
-                                    }
-                                }
-                                return visitor.done();
-                            }
-                        }
-                );
-
+                new GraphvizWriter().emit(output, new SimpleWalker(dbServices));
             };
 
             return Response.ok(stream).build();
@@ -95,5 +67,126 @@ public class ExportGraph {
         }
 
         return Response.serverError().build();
+    }
+
+
+    @GET
+    @Produces(MediaType.APPLICATION_SVG_XML)
+    @Path("svg")
+    public Response svg(
+            @DefaultValue("master")
+            @QueryParam("branchid") String branchid
+    ) {
+        try {
+            final DbServices dbServices = DbServicesManager.getDbServices(branchid);
+            Transaction transaction = dbServices.beginTx();
+
+            final File dot = File.createTempFile("dot", null);
+            dot.deleteOnExit();
+
+            FileOutputStream fileOutputStream = new FileOutputStream(dot);
+            new GraphvizWriter().emit(fileOutputStream, new SimpleWalker(dbServices));
+            fileOutputStream.close();
+
+
+            ProcessBuilder builder = new ProcessBuilder("dot", "-Tsvg", dot.getAbsolutePath());
+            builder.redirectErrorStream(true);
+            Process process = builder.start();
+
+            final InputStream inputStream = process.getInputStream();
+            final BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
+
+            StreamingOutput stream = output -> {
+                int read = bufferedInputStream.read();
+                while (read != -1) {
+                    output.write(read);
+                    read = bufferedInputStream.read();
+                }
+                output.flush();
+                bufferedInputStream.close();
+            };
+
+            return Response.ok(stream).build();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return Response.serverError().build();
+    }
+
+    @GET
+    @Produces("imgae/png")
+    @Path("png")
+    public Response png(
+            @DefaultValue("master")
+            @QueryParam("branchid") String branchid
+    ) {
+        try {
+            final DbServices dbServices = DbServicesManager.getDbServices(branchid);
+            Transaction transaction = dbServices.beginTx();
+
+            final File dot = File.createTempFile("dot", null);
+            dot.deleteOnExit();
+
+            FileOutputStream fileOutputStream = new FileOutputStream(dot);
+            new GraphvizWriter().emit(fileOutputStream, new SimpleWalker(dbServices));
+            fileOutputStream.close();
+
+
+            ProcessBuilder builder = new ProcessBuilder("dot", "-Tpng", dot.getAbsolutePath());
+            builder.redirectErrorStream(true);
+            Process process = builder.start();
+
+            final InputStream inputStream = process.getInputStream();
+            final BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
+
+            StreamingOutput stream = output -> {
+                int read = bufferedInputStream.read();
+                while (read != -1) {
+                    output.write(read);
+                    read = bufferedInputStream.read();
+                }
+                output.flush();
+                bufferedInputStream.close();
+            };
+
+            return Response.ok(stream).build();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return Response.serverError().build();
+    }
+
+    protected class SimpleWalker extends Walker {
+        private final DbServices dbServices;
+
+        public SimpleWalker(DbServices dbServices) {
+            this.dbServices = dbServices;
+        }
+
+        @Override
+        public <R, E extends Throwable> R accept(Visitor<R, E> visitor) throws E {
+            for (Node node : dbServices.graphDb.getAllNodes()) {
+                if (node.hasLabel(Label.label("CompilationUnit"))) {
+                    continue;
+                }
+                if (node.hasLabel(Label.label("SourceSpan"))) {
+                    continue;
+                }
+                if (node.hasLabel(Label.label("SourceLocation"))) {
+                    continue;
+                }
+
+                visitor.visitNode(node);
+                for (Relationship edge : node.getRelationships(Direction.OUTGOING)) {
+                    if (edge.isType(RelationshipType.withName("location"))) {
+                        continue;
+                    }
+                    visitor.visitRelationship(edge);
+                }
+            }
+            return visitor.done();
+        }
     }
 }
