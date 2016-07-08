2f09d9d45009d8550ca63c16aa27b97624913e3c
Dániel Stein <daniel@stein.hu>
2016-07-08 14:59:26 +0200

    Failsafe parallel CFG build

--- src/main/java/hu/bme/mit/codemodel/rifle/resources/BuildCallGraph.java
+++ src/main/java/hu/bme/mit/codemodel/rifle/resources/BuildCallGraph.java
@@ -3,8 +3,12 @@ package hu.bme.mit.codemodel.rifle.resources;
 import hu.bme.mit.codemodel.rifle.utils.DbServices;
 import hu.bme.mit.codemodel.rifle.utils.DbServicesManager;
 import hu.bme.mit.codemodel.rifle.utils.ResourceReader;
+import net.jodah.failsafe.Failsafe;
+import net.jodah.failsafe.FailsafeFuture;
+import net.jodah.failsafe.RetryPolicy;
 import org.neo4j.graphdb.Result;
 import org.neo4j.graphdb.Transaction;
+import org.neo4j.kernel.DeadlockDetectedException;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -12,7 +16,10 @@ import javax.ws.rs.core.Response;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.*;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 /**
@@ -20,6 +27,10 @@ import java.util.stream.Collectors;
  */
 @Path("buildcfg")
 public class BuildCallGraph {
+
+    protected static RetryPolicy retryPolicy = new RetryPolicy()
+            .retryOn(DeadlockDetectedException.class)
+            .withDelay(1, TimeUnit.SECONDS);
 
     protected final static List<String> QUERYNAMES = Arrays.asList(
             "ListNoItem",
@@ -46,55 +57,49 @@ public class BuildCallGraph {
     ) {
 
         final DbServices dbServices = DbServicesManager.getDbServices(branchid);
-        ExecutorService executorService = Executors.newCachedThreadPool();
+        ScheduledExecutorService executorService = Executors.newScheduledThreadPool(4);
 
-        List<Callable<String>> tasks = new ArrayList<>();
+        List<FailsafeFuture<String>> futures = new ArrayList<>();
 
         for (int i = 0; i < QUERYNAMES.size(); i++) {
             String name = QUERYNAMES.get(i);
             String query = QUERIES.get(i);
 
-            Callable<String> callable = () -> {
-                Transaction tx = dbServices.beginTx();
-                StringBuilder builder = new StringBuilder();
+            futures.add(
+                    Failsafe.with(retryPolicy)
+                            .with(executorService)
+                            .get(() -> {
+                                StringBuilder builder = new StringBuilder();
 
-                try {
-                    builder.append('\n').append(name).append('\n');
-                    final Result result = dbServices.graphDb.execute(query);
-                    builder.append(result.resultAsString()).append('\n');
+                                try (Transaction tx = dbServices.beginTx()) {
+                                    builder.append('\n').append(name).append('\n');
+                                    final Result result = dbServices.graphDb.execute(query);
+                                    builder.append(result.resultAsString()).append('\n');
 
-                    tx.success();
-                } catch (Exception e) {
-                    System.err.println(name);
-                    e.printStackTrace();
+                                    tx.success();
+                                } catch (Exception e) {
+                                    System.err.println(name);
+                                    e.printStackTrace();
 
-                    builder.append(name).append('\n').append(e.toString()).append('\n');
-                    throw new WebApplicationException(e);
-                } finally {
-                    if (tx != null) {
-                        tx.close();
-                    }
-                }
+                                    builder.append(name).append('\n').append(e.toString()).append('\n');
+                                    throw new WebApplicationException(e);
+                                }
 
-                return builder.toString();
-            };
-
-            tasks.add(callable);
+                                return builder.toString();
+                            })
+            );
         }
 
         try {
-            final List<Future<String>> futures = executorService.invokeAll(tasks);
             StringBuilder builder = new StringBuilder();
 
-            for (Future<String> future : futures) {
+            for (FailsafeFuture<String> future : futures) {
                 builder.append(future.get());
                 builder.append('\n');
             }
 
             return Response.ok(builder.toString()).build();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
+        } catch (InterruptedException | ExecutionException e) {
             e.printStackTrace();
         }
 
