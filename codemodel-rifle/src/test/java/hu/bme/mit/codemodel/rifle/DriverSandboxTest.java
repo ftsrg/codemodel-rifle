ec6fc9cdab6bdf0e25ea97aad4b75e474340594c
Gabor Szarnyas <szarnyasg@gmail.com>
2017-01-27 21:04:35 +0100

    Update Neo4j initialization script to circumvent authentication

--- codemodel-rifle/src/test/java/hu/bme/mit/codemodel/rifle/DriverSandboxTest.java
+++ codemodel-rifle/src/test/java/hu/bme/mit/codemodel/rifle/DriverSandboxTest.java
@@ -2,7 +2,6 @@ package hu.bme.mit.codemodel.rifle;
 
 import org.junit.Test;
 import org.neo4j.driver.v1.StatementResult;
-import org.neo4j.driver.v1.exceptions.ClientException;
 import org.neo4j.driver.v1.types.Node;
 
 import hu.bme.mit.codemodel.rifle.database.DbServices;
@@ -12,16 +11,11 @@ public class DriverSandboxTest {
 
     @Test
     public void test() {
-        try {
-            DbServices dbServices = DbServicesManager.getDbServices("master");
+        DbServices dbServices = DbServicesManager.getDbServices("master");
 
-            dbServices.beginTx();
-            StatementResult result = dbServices.execute("CREATE (n) RETURN n");
-            Node x = result.next().get(0).asNode();
-            System.out.println(x);
-        } catch (ClientException e) {
-            e.printStackTrace();
-            System.out.println(e.getMessage());
-        }
+        dbServices.beginTx();
+        StatementResult result = dbServices.execute("CREATE (n) RETURN n");
+        Node x = result.next().get(0).asNode();
+        System.out.println(x);
     }
 }
--- scripts/init-neo4j.sh
+++ scripts/init-neo4j.sh
@@ -4,8 +4,10 @@ set -e # exit with nonzero exit code if anything fails
 
 cd "$( cd "$( dirname "$0" )" && pwd )/../"
 
-NEO4J_VERSION=neo4j-community-3.1.1
-NEO4J_DIR=neo4j-db
+# if the NEO4J_HOME environment variable is set, the bin/neo4j script starts that instance
+export NEO4j_HOME=
+export NEO4J_VERSION=neo4j-community-3.1.1
+export NEO4J_DIR=neo4j-db
 
 echo Downloading and initializing a Neo4j server instance
 echo Warning: authentication is turned off for the server
@@ -13,4 +15,4 @@ echo Warning: authentication is turned off for the server
 curl https://neo4j.com/artifact.php?name=$NEO4J_VERSION-unix.tar.gz | tar xz
 mv $NEO4J_VERSION $NEO4J_DIR
 sed -i.bak "s/#dbms.security.auth_enabled=false/dbms.security.auth_enabled=false/g" $NEO4J_DIR/conf/neo4j.conf
-$NEO4J_DIR/bin/neo4j start
+$NEO4J_DIR/bin/neo4j restart
