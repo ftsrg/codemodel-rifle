2f3ea009182a4a58009cdda78cc3f49732a7b456
Gabor Szarnyas <szarnyasg@gmail.com>
2017-01-27 19:07:17 +0100

    Split project, move visualization components to a separate projects

---
+++ codemodel-visualization/src/main/java/hu/bme/mit/codemodel/rifle/resources/visualization/CfgWalker.java
@@ -0,0 +1,61 @@
+package hu.bme.mit.codemodel.rifle.resources.visualization;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.neo4j.graphdb.Direction;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Relationship;
+import org.neo4j.graphdb.Result;
+import org.neo4j.walk.Visitor;
+import org.neo4j.walk.Walker;
+
+import hu.bme.mit.codemodel.rifle.database.DbServices;
+
+// based on org.neo4j.walk.Walker.crosscut()
+public class CfgWalker extends Walker {
+
+    private final Set<Node> nodes = new HashSet<>();
+    List<String> relationships = Arrays.asList(":`_end`", ":`_normal`", ":`_next`", ":`_true`", ":`_false`");
+    List<String> relationshipLabels = relationships.stream()
+            .map(before -> before.substring(2, before.length() - 1))
+            .collect(Collectors.toList());
+
+    public CfgWalker(DbServices dbServices) {
+        final Result result = dbServices.execute(
+                String.format(
+            		"MATCH (a)-[%s]->(b) RETURN id(a) as a, id(b) as b",
+            		String.join("|", relationships)
+        		)
+        );
+
+        while (result.hasNext()) {
+            final Map<String, Object> next = result.next();
+            nodes.add(dbServices.getNodeById(Long.valueOf(next.get("a").toString())));
+            nodes.add(dbServices.getNodeById(Long.valueOf(next.get("b").toString())));
+        }
+    }
+
+    @Override
+    public <R, E extends Throwable> R accept(Visitor<R, E> visitor) throws E {
+        //filternodes:
+        for (Node node : nodes) {
+            visitor.visitNode(node);
+            for (Relationship relationship : node.getRelationships(Direction.OUTGOING)) {
+                if (nodes.contains(relationship.getOtherNode(node))) {
+
+                    if (!relationshipLabels.contains(relationship.getType().name())) {
+                        continue;
+                    }
+
+                    visitor.visitRelationship(relationship);
+                }
+            }
+        }
+        return visitor.done();
+    }
+}
---
+++ codemodel-visualization/src/main/java/hu/bme/mit/codemodel/rifle/resources/visualization/ExportGraph.java
@@ -0,0 +1,144 @@
+package hu.bme.mit.codemodel.rifle.resources.visualization;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.visualization.graphviz.GraphvizWriter;
+import org.neo4j.walk.Walker;
+
+import com.google.common.io.ByteStreams;
+
+import hu.bme.mit.codemodel.rifle.database.DbServices;
+
+/**
+ * Created by steindani on 3/2/16.
+ */
+public class ExportGraph {
+
+    final OutputStream out = System.out;
+    final GraphvizWriter writer = new GraphvizWriter();
+    final DbServices dbServices = null;
+
+    public void full(String branchid) throws IOException {
+        Transaction transaction = dbServices.beginTx();
+        writer.emit(out, Walker.fullGraph(dbServices.getGraphDb()));
+    }
+
+    public void simple(String branchid) throws IOException {
+        Transaction transaction = dbServices.beginTx();
+        writer.emit(out, new SimpleWalker(dbServices));
+    }
+
+    public void svg(String branchid, long nodeid, boolean simple, boolean cfg) throws IOException {
+        Transaction transaction = dbServices.beginTx();
+
+        final File dot = File.createTempFile("dot", null);
+        dot.deleteOnExit();
+
+        Walker walker;
+
+        if (nodeid != -1) {
+            walker = new SubgraphWalker(dbServices, nodeid, simple, cfg);
+        } else {
+            walker = new SimpleWalker(dbServices);
+        }
+
+        NewlineFilterStream fileOutputStream = new NewlineFilterStream(new FileOutputStream(dot));
+        new GraphvizWriter().emit(fileOutputStream, walker);
+        fileOutputStream.close();
+
+        ProcessBuilder builder = new ProcessBuilder("dot", "-Tsvg", dot.getAbsolutePath());
+        builder.redirectErrorStream(true);
+        Process process = builder.start();
+
+        final InputStream inputStream = process.getInputStream();
+        final BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
+
+        ByteStreams.copy(bufferedInputStream, out);
+    }
+
+    public void dot(String branchid, long nodeid, boolean simple, boolean cfg) throws IOException {
+        Transaction transaction = dbServices.beginTx();
+
+        final File dot = File.createTempFile("dot", null);
+        dot.deleteOnExit();
+
+        Walker walker;
+
+        if (nodeid != -1) {
+            walker = new SubgraphWalker(dbServices, nodeid, simple, cfg);
+        } else {
+            walker = new SimpleWalker(dbServices);
+        }
+
+        writer.emit(out, walker);
+    }
+
+    public void png(String branchid, long nodeid, boolean simple, boolean cfg) throws IOException {
+        Transaction transaction = dbServices.beginTx();
+
+        final File dot = File.createTempFile("dot", null);
+        dot.deleteOnExit();
+
+        Walker walker;
+
+        if (nodeid != -1) {
+            walker = new SubgraphWalker(dbServices, nodeid, simple, cfg);
+        } else {
+            walker = new SimpleWalker(dbServices);
+        }
+
+        NewlineFilterStream fileOutputStream = new NewlineFilterStream(new FileOutputStream(dot));
+        new GraphvizWriter().emit(fileOutputStream, walker);
+        fileOutputStream.close();
+
+        ProcessBuilder builder = new ProcessBuilder("dot", "-Tpng", dot.getAbsolutePath());
+        builder.redirectErrorStream(true);
+        Process process = builder.start();
+
+        final InputStream inputStream = process.getInputStream();
+        final BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
+
+        ByteStreams.copy(bufferedInputStream, out);
+    }
+
+    public void cfg(String branchid) throws IOException {
+        Transaction transaction = dbServices.beginTx();
+
+        final File dot = File.createTempFile("dot", null);
+        dot.deleteOnExit();
+
+        Walker walker = new CfgWalker(dbServices);
+
+        NewlineFilterStream fileOutputStream = new NewlineFilterStream(new FileOutputStream(dot));
+        new GraphvizWriter().emit(fileOutputStream, walker);
+        fileOutputStream.close();
+
+        ProcessBuilder builder = new ProcessBuilder("dot", "-Tpng", dot.getAbsolutePath());
+        builder.redirectErrorStream(true);
+        Process process = builder.start();
+
+        final InputStream inputStream = process.getInputStream();
+        final BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
+
+        ByteStreams.copy(bufferedInputStream, out);
+    }
+
+    public void cfgdot(String branchid) throws IOException {
+        Transaction transaction = dbServices.beginTx();
+
+        final File dot = File.createTempFile("dot", null);
+        dot.deleteOnExit();
+
+        Walker walker = new CfgWalker(dbServices);
+
+        GraphvizWriter writer = new GraphvizWriter();
+        writer.emit(out, walker);
+    }
+
+}
