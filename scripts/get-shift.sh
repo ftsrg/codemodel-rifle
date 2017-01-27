8ab642f83d0d47296eed4d6d6f69698315f77f41
Gabor Szarnyas <szarnyasg@gmail.com>
2017-01-27 10:04:03 +0100

    Add Travis conf

---
+++ scripts/get-shift.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e # exit with nonzero exit code if anything fails
+
+cd "$( cd "$( dirname "$0" )" && pwd )/../../"
+git clone https://github.com/steindani/shift-java.git || true
+cd shift-java
+echo "Building shift-java in quiet mode (only prints to console in case of errors)"
+time mvn clean install -q -DskipTests
