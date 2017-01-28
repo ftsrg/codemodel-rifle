eb1fdd187852e2ca96adb3b5c3189e0b1261ed29
Gabor Szarnyas <szarnyasg@gmail.com>
2017-01-28 09:28:45 +0100

    Uncomment class

--- codemodel-rifle/src/main/java/hu/bme/mit/codemodel/rifle/resources/GetLastCommitHash.java
+++ codemodel-rifle/src/main/java/hu/bme/mit/codemodel/rifle/resources/GetLastCommitHash.java
@@ -1,49 +1,48 @@
-//package hu.bme.mit.codemodel.rifle.resources;
-//
-//import java.util.Map;
-//
-//import javax.ws.rs.DefaultValue;
-//import javax.ws.rs.GET;
-//import javax.ws.rs.Path;
-//import javax.ws.rs.Produces;
-//import javax.ws.rs.QueryParam;
-//import javax.ws.rs.core.MediaType;
-//import javax.ws.rs.core.Response;
-//
-//import org.json.JSONObject;
-//import org.neo4j.graphdb.Result;
-//import org.neo4j.graphdb.Transaction;
-//
-//import hu.bme.mit.codemodel.rifle.utils.DbServices;
-//import hu.bme.mit.codemodel.rifle.utils.DbServicesManager;
-//import hu.bme.mit.codemodel.rifle.utils.ResourceReader;
-//
-//@Path("lastcommit")
-//public class GetLastCommitHash {
-//    private static final String GET_LAST_COMMIT_HASH = ResourceReader.query("getlastcommithash");
-//
-//    @GET
-//    @Produces(MediaType.TEXT_PLAIN)
-//    public Response run(
-//            @DefaultValue("master")
-//            @QueryParam("branchid") String branchid
-//    ) {
-//        final DbServices dbServices = DbServicesManager.getDbServices(branchid);
-//        try (Transaction tx = dbServices.beginTx()) {
-//            final Result result = dbServices.execute(GET_LAST_COMMIT_HASH);
-//
-//            JSONObject response = new JSONObject();
-//            while (result.hasNext()) {
-//                Map<String, Object> next = result.next();
-//
-//                Object commitHash = next.get("commitHash");
-//                response.put("commitHash", commitHash);
-//            }
-//
-//            return Response.ok(response.toString()).build();
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        return Response.serverError().build();
-//    }
-//}
+package hu.bme.mit.codemodel.rifle.resources;
+
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.json.JSONObject;
+import org.neo4j.driver.v1.Record;
+import org.neo4j.driver.v1.StatementResult;
+import org.neo4j.driver.v1.Transaction;
+
+import hu.bme.mit.codemodel.rifle.database.DbServices;
+import hu.bme.mit.codemodel.rifle.database.DbServicesManager;
+import hu.bme.mit.codemodel.rifle.database.ResourceReader;
+
+@Path("lastcommit")
+public class GetLastCommitHash {
+    private static final String GET_LAST_COMMIT_HASH = ResourceReader.query("getlastcommithash");
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response run(
+            @DefaultValue("master")
+            @QueryParam("branchid") String branchid
+    ) {
+        final DbServices dbServices = DbServicesManager.getDbServices(branchid);
+        try (Transaction tx = dbServices.beginTx()) {
+            final StatementResult result = dbServices.execute(GET_LAST_COMMIT_HASH);
+
+            JSONObject response = new JSONObject();
+            while (result.hasNext()) {
+                Record next = result.next();
+
+                Object commitHash = next.get("commitHash");
+                response.put("commitHash", commitHash);
+            }
+
+            return Response.ok(response.toString()).build();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return Response.serverError().build();
+    }
+}
