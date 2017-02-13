59a32fc7c25edf25deb42242b4d790df5bfa0195
Gabor Szarnyas <szarnyasg@gmail.com>
2017-02-13 21:35:27 +0100

    Rename and add some resilience to the get-reactive-driver.sh script

--- scripts/get-reactive-driver.sh
+++ scripts/get-reactive-driver.sh
@@ -4,6 +4,7 @@ set -e # exit with nonzero exit code if anything fails
 
 cd "$( cd "$( dirname "$0" )" && pwd )/../.."
 
-git clone https://github.com/szarnyasg/neo4j-driver-testkit
-cd neo4j-driver-testkit
+git clone https://github.com/szarnyasg/neo4j-reactive-driver || true
+cd neo4j-reactive-driver
+git fetch
 ./gradlew publishToMavenLocal
