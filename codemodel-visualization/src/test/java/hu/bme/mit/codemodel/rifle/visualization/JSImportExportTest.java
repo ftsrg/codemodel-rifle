0ec0e721a8a20fb77bccb794f1f3b76848d7d0a4
Lucz Soma <luczsoma@gmail.com>
2017-03-26 15:16:44 +0200

    Added a basic import-export test scenario to visualization

---
+++ codemodel-visualization/src/test/java/hu/bme/mit/codemodel/rifle/visualization/JSImportExportTest.java
@@ -0,0 +1,77 @@
+package hu.bme.mit.codemodel.rifle.visualization;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import hu.bme.mit.codemodel.rifle.actions.repository.SynchronizeRepository;
+import hu.bme.mit.codemodel.rifle.actions.utils.DeleteGraph;
+import hu.bme.mit.codemodel.rifle.database.DbServices;
+import hu.bme.mit.codemodel.rifle.database.DbServicesManager;
+import hu.bme.mit.codemodel.rifle.tasks.ImportExport;
+import hu.bme.mit.codemodel.rifle.visualization.actions.ExportGraph;
+import hu.bme.mit.codemodel.rifle.visualization.database.DbServiceDecorator;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class JSImportExportTest extends TestCase {
+
+    @Before
+    @After
+    public void deleteDb() {
+        new DeleteGraph().delete(branchId);
+    }
+
+    @Test
+    public void test1() {
+        DbServices dbServices = DbServicesManager.getDbServices(branchId);
+        DbServiceDecorator dbServicesDecorator = new DbServiceDecorator(dbServices.getUnderlyingDatabaseService());
+        String path = this.getTestResourcesFolderPath("test1");
+
+        SynchronizeRepository synchronizeRepository = new SynchronizeRepository(path, branchId, sessionId);
+        synchronizeRepository.sync();
+
+        File beforePng = null;
+        File afterPng = null;
+
+        ExportGraph eg = new ExportGraph(dbServicesDecorator);
+
+        try {
+            beforePng = new File(path + File.separator + "before.png");
+            if (!beforePng.exists()) {
+                beforePng.createNewFile();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try (FileOutputStream fop = new FileOutputStream(beforePng)) {
+            eg.setOutputStream(fop);
+            eg.png(branchId, -1, true, false);
+        } catch (IOException|NullPointerException e) {
+            e.printStackTrace();
+        }
+
+        ImportExport importExport = new ImportExport();
+        importExport.importExport(branchId);
+
+        try {
+            afterPng = new File(path + File.separator + "after.png");
+            if (!afterPng.exists()) {
+                afterPng.createNewFile();
+            }
+        } catch (IOException|NullPointerException e) {
+            e.printStackTrace();
+        }
+
+        try (FileOutputStream fop = new FileOutputStream(afterPng)) {
+            eg.setOutputStream(fop);
+            eg.png(branchId, -1, true, false);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
