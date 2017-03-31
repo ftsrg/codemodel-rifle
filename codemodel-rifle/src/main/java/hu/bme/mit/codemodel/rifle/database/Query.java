f6e020c0e4485648158b3b51f1b5e58235f64081
Lucz Soma <luczsoma@gmail.com>
2017-03-31 18:23:22 +0200

    Query class create for the QueryBuilder

---
+++ codemodel-rifle/src/main/java/hu/bme/mit/codemodel/rifle/database/Query.java
@@ -0,0 +1,35 @@
+package hu.bme.mit.codemodel.rifle.database;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Query {
+    private String statementTemplate;
+    private Map<String, Object> statementParameters = new HashMap<>();
+
+    public Query() {}
+
+    public Query(String template) {
+        this.statementTemplate = template;
+    }
+
+    public Query(String template, Map<String, Object> parameters) {
+        this.statementTemplate = template;
+        this.statementParameters = parameters;
+    }
+
+    public String getStatementTemplate() {
+        return statementTemplate;
+    }
+
+    public Map<String, Object> getStatementParameters() {
+        return statementParameters;
+    }
+
+    public synchronized void append(String template, Map<String, Object> parameters) {
+        StringBuilder builder = new StringBuilder(this.statementTemplate);
+        builder.append(template);
+        this.statementTemplate = builder.toString();
+        this.statementParameters.putAll(parameters);
+    }
+}
