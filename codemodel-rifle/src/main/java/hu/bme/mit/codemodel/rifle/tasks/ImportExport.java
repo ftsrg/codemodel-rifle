9534824ba26f1af8c716f6898ee4f8564f4f369e
Lucz Soma <luczsoma@gmail.com>
2017-04-06 19:01:19 +0200

    Unnecessary result in ImportExport

--- codemodel-rifle/src/main/java/hu/bme/mit/codemodel/rifle/tasks/ImportExport.java
+++ codemodel-rifle/src/main/java/hu/bme/mit/codemodel/rifle/tasks/ImportExport.java
@@ -1,10 +1,8 @@
 package hu.bme.mit.codemodel.rifle.tasks;
 
 import java.util.Collection;
-import java.util.List;
 import java.util.logging.Logger;
 
-import org.neo4j.driver.v1.StatementResult;
 import org.neo4j.driver.v1.Transaction;
 
 import hu.bme.mit.codemodel.rifle.database.DbServices;
@@ -15,17 +13,14 @@ public class ImportExport {
     private static final Logger logger = Logger.getLogger("codemodel");
 
     public boolean importExport(String branchId) {
-
         final DbServices dbServices = DbServicesManager.getDbServices(branchId);
-//        long start = System.currentTimeMillis();
         final Collection<String> importExportQueries = ResourceReader.getImportExportQueries();
 
         try (Transaction tx = dbServices.beginTx()) {
             for (String query : importExportQueries) {
-                StatementResult result = dbServices.execute(query);
-//                logger.info(" TIME " + (System.currentTimeMillis() - start));
-//                logger.info(result.toString());
+                dbServices.execute(query);
             }
+
             tx.success();
             return true;
         } catch (Exception e) {
