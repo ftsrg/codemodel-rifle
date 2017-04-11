eae975d009d7fb8199d108cef879e7979588ef98
Lucz Soma <luczsoma@gmail.com>
2017-04-11 14:00:10 +0200

    exportAlias_importAlias impex case done

--- codemodel-visualization/src/test/resources/impex/ExportAliasTest/test_exportAlias_importAlias/importer.js
+++ codemodel-visualization/src/test/resources/impex/ExportAliasTest/test_exportAlias_importAlias/importer.js
@@ -1,1 +1,6 @@
-import { exportedName1 as name1 } from "exporter";
+import {
+    exportedName1 as importedName1,
+    exportedName2 as importedName2,
+    exportedName3 as importedName3,
+    exportedName4 as importedName4
+} from "exporter";
