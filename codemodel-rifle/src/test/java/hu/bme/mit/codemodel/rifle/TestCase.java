438cab35db4f29baa59bbcd7a3b82da4cd8a82ba
Lucz Soma <luczsoma@gmail.com>
2017-04-18 12:35:39 +0200

    TestCases can be folder prefixed

--- codemodel-rifle/src/test/java/hu/bme/mit/codemodel/rifle/TestCase.java
+++ codemodel-rifle/src/test/java/hu/bme/mit/codemodel/rifle/TestCase.java
@@ -3,14 +3,20 @@ package hu.bme.mit.codemodel.rifle;
 import java.io.File;
 
 public abstract class TestCase {
-
-    protected static final String branchId = "dummyTest";
-    protected static final String sessionId = "dummyTest";
+    protected static final String branchId = "dummyTestExport";
+    protected static final String sessionId = "dummyTestExport";
+    protected String testFolderPrefix = "";
 
     protected String getTestResourcesFolderPath(String testMethodName) {
         try {
             String testResourceFolderWithinResources = this.getClass().getSimpleName() + File.separator +
                 testMethodName;
+
+            if (!this.testFolderPrefix.equals("")) {
+                testResourceFolderWithinResources = this.testFolderPrefix + File.separator +
+                    testResourceFolderWithinResources;
+            }
+
             String path = this.getClass().getClassLoader().getResource(testResourceFolderWithinResources).getPath();
             return path;
         } catch (NullPointerException e) {
@@ -18,5 +24,4 @@ public abstract class TestCase {
             return null;
         }
     }
-
 }
