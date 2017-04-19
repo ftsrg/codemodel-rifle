4259d2bc9121494af0a582614bf64da8c52a6c1d
Lucz Soma <luczsoma@gmail.com>
2017-04-19 20:41:20 +0200

    Test resource was missing

---
+++ codemodel-visualization/src/test/resources/impex/ExportDefaultNameTest/test_exportDefaultName_importAlias/exporter.js
@@ -0,0 +1,13 @@
+class name1 {
+    constructor(x) {
+        this.x = x;
+    }
+}
+
+// function name1() {
+//     return "name1Value";
+// }
+
+// let name1 = "name1Value";
+
+export default name1;
